58dfa268efde2dc68c9de65ab278cdc701857f1e
LucasACortes <corteslucasa@outlook.com>
2019-02-22 16:04:30 -0300

    Cambios parciales

---
+++ src/com/eBolivar/dao/localidad/interfaces/ILocalidadRepository.java
@@ -0,0 +1,7 @@
+package com.eBolivar.dao.localidad.interfaces;
+
+/**
+ * Created by Lucas Cortés on 22/02/2019.
+ */
+public interface ILocalidadRepository {
+}
---
+++ src/com/eBolivar/service/localidad/LocalidadService.java
@@ -0,0 +1,7 @@
+package com.eBolivar.dao;
+
+/**
+ * Created by Lucas Cortés on 22/02/2019.
+ */
+public class LocalidadService {
+}
